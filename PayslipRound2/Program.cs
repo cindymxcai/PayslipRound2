6d2b1b4107d40a3b5754ee40c4a1040e1bcc1ff3
Cindy Cai <cindymxcai@gmail.com>
2020-12-02 16:40:06 +1300

    parse csv file to user info

---
+++ PayslipRound2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayslipRound2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PayslipTests/CsvParser.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.FileIO;
+
+namespace PayslipTests
+{
+    public class CsvParser
+    {
+        private TextFieldParser _textFieldParser;
+
+        private string[] Fields { get; }
+
+        public CsvParser(string path)
+        {
+           Fields = SplitRow(path).ToArray();
+        }
+
+        private IEnumerable<string> SplitRow(string path)
+        {
+            _textFieldParser = new TextFieldParser(path) {TextFieldType = FieldType.Delimited};
+            _textFieldParser.SetDelimiters(",");
+           return _textFieldParser.ReadFields();
+        }
+
+        public UserInformation GetUserInformation()
+        {
+            var dateInputs = Fields[4].Split(" ");
+            dateInputs.ToList().Remove("-");
+
+            var userInfo = new UserInformation
+            {
+                Name = Fields[0],
+                Surname = Fields[1],
+                Salary = int.Parse(Fields[2]),
+                SuperRate = int.Parse(Fields[3].Replace("%","")),
+                StartDate = ParsePaymentStart(dateInputs),
+                EndDate = ParsePaymentEnd(dateInputs)
+            };
+            
+            return userInfo;
+        }
+
+        private static string ParsePaymentEnd(string[] dateInputs)
+        {
+            return $"{dateInputs[3]}, {dateInputs[4]}";
+            }
+
+        private static string ParsePaymentStart(string[] dateInputs)
+        {
+            return $"{dateInputs[0]}, {dateInputs[1]}";
+        }
+    }
+}
